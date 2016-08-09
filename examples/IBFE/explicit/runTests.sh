f342f7394b56c073a9ff0d1318c20f61250659ee
Elijah <deleeke@gmail.com>
2016-08-09 12:03:09 -0400

    Add ex6 tests and make seperate input files for running tests

--- examples/IBFE/explicit/runTests.sh
+++ examples/IBFE/explicit/runTests.sh
@@ -13,12 +13,12 @@ do
     if [ -f "test2d" ];
     then
         echo "************Running "test2d"************"
-        ./test2d input2d --gtest_filter=*.2d
+        ./test2d input2d.test --gtest_filter=*.2d
     fi
     if [ -f "test3d" ];
     then
         echo "************Running "test3d"************"
-        ./test3d input3d --gtest_filter=*.3d
+        ./test3d input3d.test --gtest_filter=*.3d
     fi
     cd ..
 done
